fa783c7b193a40c65fc21e0d442e4057b9897543
Matthew Franglen <matthew@franglen.org>
2017-10-07 20:36:50 +0100

    Start defining coordinate systems and worlds
    This is optimised for mapping rather than for efficiency.
    This is really a toy problem rather than something serious after all.

--- src/main/scala/com/matthew/gol/GameOfLife.scala
+++ src/main/scala/com/matthew/gol/GameOfLife.scala
@@ -26,3 +26,24 @@ case class Window(target: Cell, neighbours: Seq[Cell]) {
   val livingNeighbours: Int =
     neighbours.filter(_ eq LiveCell).length
 }
+
+sealed trait Position[P <: Position[P]] {
+  def distanceTo(other: P): Int
+}
+
+case class Position2d(x: Int, y: Int) extends Position[Position2d] {
+  def distanceTo(other: Position2d): Int =
+    Math.max(Math.abs(x - other.x), Math.abs(y - other.y))
+}
+
+case class PositionNd(coordinates: Seq[Int]) extends Position[PositionNd] {
+  def distanceTo(other: PositionNd): Int =
+    coordinates
+      .zip(other.coordinates)
+      .map({ case (a, b) => Math.abs(a - b) })
+      .max
+}
+
+case class WorldCell[P <: Position[P]](cell: Cell, position: P)
+
+case class World[P <: Position[P]](cells: Seq[WorldCell[P]])
