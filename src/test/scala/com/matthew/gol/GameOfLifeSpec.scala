80508e5308657893c779b1b942864daa4583feb6
Matthew Franglen <matthew@franglen.org>
2017-10-08 22:10:30 +0100

    Add tests for the n-dimensional position

--- src/test/scala/com/matthew/gol/GameOfLifeSpec.scala
+++ src/test/scala/com/matthew/gol/GameOfLifeSpec.scala
@@ -38,7 +38,7 @@ class SpecificationSpec extends FlatSpec with Matchers {
   }
 }
 
-class WorldSpec extends FlatSpec with Matchers {
+class Position2dSpec extends FlatSpec with Matchers {
   "The World class" should "create the next barren world" in {
     implicit val spec = Specification(Seq(3), Seq(2, 3))
     val cell = WorldCell(LiveCell, Position2d(0, 0))
@@ -78,6 +78,52 @@ class WorldSpec extends FlatSpec with Matchers {
     val world = World(cells)
 
     val cell = WorldCell(LiveCell, Position2d(0, 0))
+    val expected = World(Seq(cell))
+
+    world.next shouldEqual expected
+  }
+}
+
+class PositionNdSpec extends FlatSpec with Matchers {
+  "The World class" should "create the next barren world" in {
+    implicit val spec = Specification(Seq(3), Seq(2, 3))
+    val cell = WorldCell(LiveCell, PositionNd(Seq(0, 0)))
+    val world = World(Seq(cell))
+
+    world.next shouldEqual World(Seq())
+  }
+
+  "The World class" should "create the next living world" in {
+    implicit val spec = Specification(Seq(3), Seq(0, 2, 3))
+    val cell = WorldCell(LiveCell, PositionNd(Seq(0, 0)))
+    val world = World(Seq(cell))
+
+    world.next shouldEqual world
+  }
+
+  "The World class" should "create the next sustaining world" in {
+    implicit val spec = Specification(Seq(3), Seq(2, 3))
+    val cells = Seq(
+      WorldCell(LiveCell, PositionNd(Seq(1, 0))),
+      WorldCell(LiveCell, PositionNd(Seq(0, 1))),
+      WorldCell(LiveCell, PositionNd(Seq(-1, 0))),
+      WorldCell(LiveCell, PositionNd(Seq(0, -1)))
+    )
+    val world = World(cells)
+
+    world.next shouldEqual world
+  }
+
+  "The World class" should "create the next thriving world" in {
+    implicit val spec = Specification(Seq(3), Seq(2, 3))
+    val cells = Seq(
+      WorldCell(LiveCell, PositionNd(Seq(1, 1))),
+      WorldCell(LiveCell, PositionNd(Seq(-1, 1))),
+      WorldCell(LiveCell, PositionNd(Seq(-1, -1)))
+    )
+    val world = World(cells)
+
+    val cell = WorldCell(LiveCell, PositionNd(Seq(0, 0)))
     val expected = World(Seq(cell))
 
     world.next shouldEqual expected
