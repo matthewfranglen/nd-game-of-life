06bb956f5439fc6ec0f43c375aa1cd340a29a480
Matthew Franglen <matthew@franglen.org>
2017-10-08 20:51:18 +0100

    Define the neighbour calculations
    This was fun

--- src/main/scala/com/matthew/gol/GameOfLife.scala
+++ src/main/scala/com/matthew/gol/GameOfLife.scala
@@ -39,15 +39,36 @@ case class Window(target: Cell, neighbours: Seq[Cell]) {
 }
 
 sealed trait Position[P <: Position[P]] {
+  def neighbours: Seq[Position[P]]
   def distanceTo(other: P): Int
 }
 
 case class Position2d(x: Int, y: Int) extends Position[Position2d] {
+  val neighbours = Seq(1,0,-1).zip(Seq(1,0,-1))
+    .filter({ case (xOffset, yOffset) => xOffset != 0 && yOffset != 0 })
+    .map({ case (xOffset, yOffset) => Position2d(x + xOffset, y + yOffset) })
+
   def distanceTo(other: Position2d): Int =
     Math.max(Math.abs(x - other.x), Math.abs(y - other.y))
 }
 
 case class PositionNd(coordinates: Seq[Int]) extends Position[PositionNd] {
+  val neighbours = {
+    val offsets = coordinates.map(_ => Seq(-1, 0, 1))
+
+    // This creates the list of permutations of offsets,
+    // filters the single one which is no offset (all dimension offsets are zero),
+    // and then applies them to the coordinates to produce the neighbouring coordinates.
+    // Once that has been done they can be converted into positions.
+    offsets.tail
+      .foldLeft(offsets.head.map(Seq(_)))(
+        (existingOffsets, dimensionOffsets) => existingOffsets.flatMap(offset => dimensionOffsets.map(offset :+ _))
+      )
+      .filter(_.exists(_ != 0))
+      .map(coordinates.zip(_).map({ case (coordinate, offset) => coordinate + offset }))
+      .map(PositionNd(_))
+  }
+
   def distanceTo(other: PositionNd): Int =
     coordinates
       .zip(other.coordinates)
