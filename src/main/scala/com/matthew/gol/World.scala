2cb2572e15a2a3c9bcd2d56833288b9138d8d86e
Matthew Franglen <matthew@franglen.org>
2017-10-09 19:05:27 +0100

    Refactor world code

--- src/main/scala/com/matthew/gol/World.scala
+++ src/main/scala/com/matthew/gol/World.scala
@@ -1,46 +1,61 @@
 package com.matthew.gol
 
-case class WorldCell[P <: Position[P]](cell: Cell, position: P) {
-  val isLive = cell.isLive
+case class World[P <: Position[P]](cells: Seq[WorldCell[P]]) {
+  private val cellsByPosition: Map[P, WorldCell[P]] =
+    cells.map { c => c.position -> c }
+      .toMap
+
+  def next(implicit spec: Specification): World[P] = {
+    val liveCells = cells.filter { _.isLive }
+    val deadCells = {
+      val neighbours = cells.flatMap { _.position.neighbours }
+        .distinct
+      val deadNeighbours = neighbours filter { isDead _ }
+
+      deadNeighbours.map { WorldCell(DeadCell, _) }
+    }
+    val nextLiveCells = {
+      val allCells = Seq(liveCells, deadCells).flatten
+      val cellWindows = allCells map { WorldWindow.toWindow(_, cellsByPosition) }
+      val nextCells = cellWindows map { _.toCell }
+
+      nextCells filter { _.isLive }
+    }
+
+    World(nextLiveCells)
+  }
+
+  private def isDead(position: P): Boolean =
+    cellsByPosition.get(position)
+      .map { _.isDead }
+      .getOrElse(true)
 }
 
-case class Window(target: Cell, neighbours: Seq[Cell]) {
+case class WorldCell[P <: Position[P]](cell: Cell, position: P) {
+  val isLive = cell.isLive
+
+  val isDead = cell.isDead
+}
+
+private case class Window(target: Cell, neighbours: Seq[Cell]) {
   def toCell(implicit spec: Specification): Cell =
     if (spec.willBeLive(target, neighbours)) LiveCell else DeadCell
 }
 
-case class WorldWindow[P <: Position[P]](window: Window, position: P) {
+private case class WorldWindow[P <: Position[P]](window: Window, position: P) {
   def toCell(implicit spec: Specification): WorldCell[P] =
     WorldCell(window.toCell, position)
 }
 
-case class World[P <: Position[P]](cells: Seq[WorldCell[P]]) {
-  require(cells.forall(_.isLive), "This implementation handles sparse worlds that contain only live cells")
+private object WorldWindow {
+  def toWindow[P <: Position[P]](cell: WorldCell[P], cells: Map[P, WorldCell[P]]): WorldWindow[P] =
+    toWindow(cell, getNeighbours(cell, cells))
 
-  val cellsByPosition = cells.map(worldCell => worldCell.position -> worldCell).toMap
+  private def toWindow[P <: Position[P]](cell: WorldCell[P], neighbours: Seq[Cell]): WorldWindow[P] =
+    WorldWindow(Window(cell.cell, neighbours), cell.position)
 
-  def next(implicit spec: Specification): World[P] = {
-    val liveWindows = cells.map(toWindow _)
-    val deadWindows = cells.flatMap(_.position.neighbours)
-      .distinct
-      .filterNot(cellsByPosition contains _)
-      .map(WorldCell(DeadCell, _))
-      .map(toWindow _)
-
-    val worldCells = Seq(liveWindows, deadWindows)
-      .flatten
-      .map(_.toCell)
-      .filter(_.isLive)
-
-    World(worldCells)
-  }
-
-  def toWindow(worldCell: WorldCell[P]): WorldWindow[P] = {
-    val neighbours = worldCell.position.neighbours
-      .map(cellsByPosition get _)
-      .filter(_.isDefined)
-      .map(_.get.cell)
-    val window = Window(worldCell.cell, neighbours)
-    WorldWindow(window, worldCell.position)
-  }
+  private def getNeighbours[P <: Position[P]](cell: WorldCell[P], cells: Map[P, WorldCell[P]]): Seq[Cell] =
+    cell.position.neighbours
+      .map { cells get _ }
+      .map { option => if (option.isDefined) option.get.cell else DeadCell }
 }
