c4d01383dc754455f12d68241c00de08690f6406
Matthew Franglen <matthew@franglen.org>
2017-10-11 20:03:25 +0100

    Use game of life rules by default

--- src/main/scala/com/matthew/server/GameOfLifeModule.scala
+++ src/main/scala/com/matthew/server/GameOfLifeModule.scala
@@ -3,6 +3,6 @@ package com.matthew.server
 import com.twitter.inject.TwitterModule
 
 object GameOfLifeModule extends TwitterModule {
-  val born = flag("born", "2", "Dead Cells become Live when they have this many Live neighbours")
+  val born = flag("born", "3", "Dead Cells become Live when they have this many Live neighbours")
   val survives = flag("survives", "2,3", "Live Cells survive when they have this many Live neighbours")
 }
