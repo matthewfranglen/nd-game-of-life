e1f743f2ac56ceb03b11d8ce7d527b396312648d
Matthew Franglen <matthew@franglen.org>
2017-10-20 21:06:28 +0100

    Fixes #4: Write tests for DimensionList

--- backend/src/test/scala/com/matthew/gol/PositionSpec.scala
+++ backend/src/test/scala/com/matthew/gol/PositionSpec.scala
@@ -54,3 +54,40 @@ class PositionNdSpec extends FlatSpec with Matchers {
     neighbours.distinct.length shouldEqual 26
   }
 }
+
+class DimensionListSpec extends FlatSpec with Matchers {
+  "The Dimension List class" should "measure distances orthogonally" in {
+    val one = Dimension(0, Dimension(0, End()))
+    val two = Dimension(1, Dimension(2, End()))
+
+    one distanceTo two shouldEqual 2
+  }
+
+  "The Dimension List class" should "have 0 distinct neighbours for 0 dimensional positions" in {
+    val neighbours = End.neighbours
+
+    neighbours.length shouldEqual 0
+    neighbours.distinct.length shouldEqual 0
+  }
+
+  "The Dimension List class" should "have 2 distinct neighbours for 1 dimensional positions" in {
+    val neighbours = Dimension(0, End()).neighbours
+
+    neighbours.length shouldEqual 2
+    neighbours.distinct.length shouldEqual 2
+  }
+
+  "The Dimension List class" should "have 8 distinct neighbours for 2 dimensional positions" in {
+    val neighbours = Dimension(0, Dimension(0, End())).neighbours
+
+    neighbours.length shouldEqual 8
+    neighbours.distinct.length shouldEqual 8
+  }
+
+  "The Dimension List class" should "have 26 distinct neighbours for 3 dimensional positions" in {
+    val neighbours = Dimension(0, Dimension(0, Dimension(0, End()))).neighbours
+
+    neighbours.length shouldEqual 26
+    neighbours.distinct.length shouldEqual 26
+  }
+}
